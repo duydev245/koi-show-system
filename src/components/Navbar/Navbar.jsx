ddfa0f7bcf96e1ea8a8b08917d1b49bb4320c6ce
duydev245 <danniduy.work@gmail.com>
2024-11-12 23:04:47 +0700

    Change navbar content

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
                         <li className="block px-4 py-2 text-base text-gray-700 hover:bg-gray-100 cursor-pointer"
                           onClick={() => { navigate(PATH.PROFILE) }}
                         >
-                          Dashboard
+                          Your profile dashboard
                         </li>
                         <li className="block px-4 py-2 text-base text-gray-700 hover:bg-gray-100 cursor-pointer"
                           onClick={() => { navigate(PATH.USER_CART) }}
